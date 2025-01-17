47fa9dd0bef42a19bd5cd178b98d10794bec490c
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-01-17 14:07:11 -0500

    CSS issues
    Crystal css not loading even though 200 and 304 status codes shown when stylesheet loaded.

--- src/invenv6.cr
+++ src/invenv6.cr
@@ -1,10 +1,13 @@
 require "kemal"
+
 module Invenv6
   VERSION = "0.1.0"
+end
 
-  get "/" do
-    "Hello world!"
-  end
+serve_static({"gzip" => false})
+
+get "/" do |env|
+  render "src/views/login.ecr"
 end
 
 Kemal.run
