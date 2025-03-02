a5160223a6695d574ea883c2c7894fa7d4ab6384
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-03-02 01:19:58 -0500

    Update invenv6.cr
    talks to freeIPA server,issue with user formatting.

--- src/invenv6.cr
+++ src/invenv6.cr
@@ -1,6 +1,7 @@
 require "kemal"
 require "kemal-basic-auth"
-
+require "http/client"
+require "json"
 module Invenv6
   VERSION = "0.1.0"
 end
@@ -15,10 +16,40 @@ class CustomAuthHandler < Kemal::BasicAuth::Handler
   end
 end
 
-#add handler
+# add handler
 Kemal.config.add_handler(CustomAuthHandler.new("admin", "password"))
 
 serve_static({"gzip" => false})
+
+private def validatecreds(username, pass) # handles yapping to the FREEIPA server
+  puts "#{username}"
+  puts "#{pass}"
+
+  # login_url = "https://citadel.cif.rochester.edu/ipa/session/login_password"
+  login_url = "https://ipa.demo1.freeipa.org/ipa/session/login_password"
+  body = {"user" => username,"password"=> pass}.to_json
+
+  headers = HTTP::Headers{
+    "Content-Type" => "application/x-www-form-urlencoded",
+    "Referer"      => login_url,
+    "Accept" => "response/json",
+  }
+
+  response = HTTP::Client.post(
+    url: login_url,
+    headers: headers,
+    body:body,
+  )
+
+  puts "Status #{response.status_code}"
+  puts "Response #{response.body}"
+end
+
+post "/login" do |env| # handling post for pass
+  username = env.params.body["username"].as(String)
+  password = env.params.body["password"].as(String)
+  validatecreds(username, password)
+end
 
 get "/" do |env|
   render "src/views/login.ecr"
