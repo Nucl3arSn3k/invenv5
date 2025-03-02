ef48fbc92db470c45da0526b42c4e02339e0199f
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-03-01 20:05:49 -0500

    Handler functional

--- src/invenv6.cr
+++ src/invenv6.cr
@@ -1,8 +1,22 @@
 require "kemal"
+require "kemal-basic-auth"
 
 module Invenv6
   VERSION = "0.1.0"
 end
+
+# Create custom handler
+class CustomAuthHandler < Kemal::BasicAuth::Handler
+  only ["/dashboard", "/homepage"]
+
+  def call(context)
+    return call_next(context) unless only_match?(context)
+    super
+  end
+end
+
+#add handler
+Kemal.config.add_handler(CustomAuthHandler.new("admin", "password"))
 
 serve_static({"gzip" => false})
 
@@ -10,9 +24,12 @@ get "/" do |env|
   render "src/views/login.ecr"
 end
 
-
 get "/homepage" do |env|
   render "src/views/table.ecr"
+end
+
+get "/dashboard" do |env|
+  "Dashboard content here"
 end
 
 Kemal.run
